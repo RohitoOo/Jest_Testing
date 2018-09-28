71353c3e33a1d704b0423268df79590806cd87d6
Rohit Bhambhani <rohit.m.bhambhani@gmail.com>
2018-09-28 16:02:36 +0530

    Additional Tests

--- functions.js
+++ functions.js
@@ -1,6 +1,14 @@
 const functions = { 
     add: (num1,num2) => num1 + num2,
-    isNull: () => undefined
+    isNull: () => undefined,
+    checkValue: (x) => x,
+    checkUser: () => {
+        let User = {
+        firstName : "Rohito", 
+        };
+        User['lastName'] = 'Bhambhani'
+        return User
+    }
 }
-
+ 
 module.exports = functions;
--- functions.test.js
+++ functions.test.js
@@ -3,3 +3,14 @@ const functions = require('./functions')
 test('Adds 2 + 2 to equal 4' , function(){
     expect(functions.isNull()).toBeUndefined();
 })
+
+test('Should Be Falsy' , function(){
+    expect(functions.checkValue(2)).toBeTruthy();
+})
+
+test("User Should have firstName of Rohito" , function(){
+    expect(functions.checkUser()).toEqual({
+        firstName: "Rohito",
+        lastName: "Bhambhani"
+    })
+})
